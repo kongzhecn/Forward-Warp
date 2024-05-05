13e580eae76023a02cd6dea572ed2f83a6f494bf
Zhe Kong <43921423+kongzhecn@users.noreply.github.com>
2024-05-05 20:08:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 work_path=$(dirname $(readlink -f $0))
 cd ${work_path}/Forward_Warp/cuda/
-conda activate pytorch
 python setup.py install | grep "error"
 cd ../../
 python setup.py install
