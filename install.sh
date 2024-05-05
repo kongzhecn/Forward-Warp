e2da4eac737aa05b005afe3a57e9cc7822166cc2
Zhe Kong <43921423+kongzhecn@users.noreply.github.com>
2024-05-05 20:33:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-work_path=$(dirname $(readlink -f $0))
-cd Forward_Warp/cuda/
+work_path=/app
+cd ${work_path}/Forward_Warp/cuda/
 python setup.py install | grep "error"
 cd ../../
 python setup.py install
